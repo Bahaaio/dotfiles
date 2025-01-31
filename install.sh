f2aa9d5ed56e901860398882e9bdc10c39832d42
BahaaMohamed98 <emv23550@gmail.com>
2025-01-31 11:02:04 +0200

    Removed bashrc changed zsh aliases

--- install.sh
+++ install.sh
@@ -2,11 +2,8 @@
 
 # Iterate through each directory in dotfiles/
 for dir in */; do
-    # Check if the directory contains files before running stow
-    if [ -d "$dir" ]; then
-        echo "Stowing $dir..."
-        stow "$dir"
-    fi
+    echo "Stowing $dir"
+    stow "$dir"
 done
 
 echo "Dotfiles installation complete!"

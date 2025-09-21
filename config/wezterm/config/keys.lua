75ccdaeae380d8f2731e5097c14c25060c9fdaed
Bahaa Mohamed <bahaa-dev@proton.me>
2025-09-21 14:53:58 +0300

    refactor: reorganize tmux structure and simplify wezterm keys
    - restructure tmux config with sections
    - add window movement shortcuts
    - improve copy-mode keybinds
    - simplify wezterm key modifiers
    - change url selction keybind to alt+shift+o
    - change transparency keybind to alt+o

--- config/wezterm/config/keys.lua
+++ config/wezterm/config/keys.lua
@@ -2,60 +2,56 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 local mod = {
-   NONE       = 'NONE',
-   CTRL       = 'CTRL',
-   SUPER      = 'ALT',
-   SHIFT      = 'SHIFT',
-   SUPER_REV  = 'ALT|CTRL',
-   CTRL_SHIFT = 'CTRL|SHIFT',
+	NONE  = "NONE",
+	SHIFT = "SHIFT",
+	ALT   = "ALT",
+	CTRL  = "CTRL",
 }
 
 return {
 	disable_default_key_bindings = true,
 	keys = {
-		-- font: resize font
+		-- resize font
 		{ key = "=", mods = mod.CTRL, action = action.IncreaseFontSize },
 		{ key = "-", mods = mod.CTRL, action = action.DecreaseFontSize },
 
-		-- misc/useful --
-		{ key = "v", mods = mod.SUPER, action = "ActivateCopyMode" },
+		-- misc
 		{ key = "F11", mods = mod.NONE, action = action.ToggleFullScreen },
 		{ key = "F12", mods = mod.NONE, action = action.ShowDebugOverlay },
+
+        -- open url
 		{
-			key = "u",
-			mods = mod.SUPER_REV,
+			key = "O", mods = mod.ALT,
 			action = wezterm.action.QuickSelectArgs({
 				label = "open url",
 				patterns = {
-					"\\((https?://\\S+)\\)",
-					"\\[(https?://\\S+)\\]",
-					"\\{(https?://\\S+)\\}",
-					"<(https?://\\S+)>",
+					"\\((https?://\\S+)\\)", "\\[(https?://\\S+)\\]",
+					"\\{(https?://\\S+)\\}", "<(https?://\\S+)>",
 					"\\bhttps?://\\S+[)/a-zA-Z0-9-]+",
 				},
 				action = wezterm.action_callback(function(window, pane)
 					local url = window:get_selection_text_for_pane(pane)
-					wezterm.log_info("opening: " .. url)
 					wezterm.open_with(url)
 				end),
 			}),
 		},
 
-		-- copy/paste --
-		{ key = "c", mods = mod.CTRL_SHIFT, action = action.CopyTo("Clipboard") },
-		{ key = "v", mods = mod.CTRL_SHIFT, action = action.PasteFrom("Clipboard") },
+		-- copy/paste
+		{ key = "C", mods = mod.CTRL, action = action.CopyTo("Clipboard") },
+		{ key = "V", mods = mod.CTRL, action = action.PasteFrom("Clipboard") },
 		{ key = "Insert", mods = mod.SHIFT, action = action.PasteFrom("Clipboard") },
 
 		-- toggle transparency
 		{
-			key = "o", mods = mod.SUPER,
+			key = "o", mods = mod.ALT,
 			action = wezterm.action_callback(function(window, _)
+				local blur_opacity = 0.85
 				local overrides = window:get_config_overrides() or {}
 
-				if overrides.window_background_opacity == 0.85 then
+				if overrides.window_background_opacity == blur_opacity then
 					overrides.window_background_opacity = 1.0
 				else
-					overrides.window_background_opacity = 0.85
+					overrides.window_background_opacity = blur_opacity
 				end
 
 				window:set_config_overrides(overrides)

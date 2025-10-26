ad7917b8a7dc3a6329e9d8a3b071bb8ac8362ba8
Bahaa Mohamed <bahaa-dev@proton.me>
2025-10-26 16:16:47 +0300

    feat(tmux-sessionizer): add ~/Documents/

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 # Script to manage tmux sessions
 
-DIRECTORIES=(~/code ~/dotfiles ~/github) # to be searched for projects
-PATHS=(~/dotfiles/ ~/.config/)           # specific paths to include
+DIRECTORIES=(~/code ~/dotfiles ~/github ~/Documents) # to be searched for projects
+PATHS=(~/dotfiles ~/.config)                         # specific paths to include
+DEPTH=2                                              # depth for searching directories
 
 if [[ $# -eq 1 ]]; then
     selected="$1"
@@ -11,7 +12,7 @@ else
     selected=$(
         {
             printf '%s\n' "${PATHS[@]}"
-            fd . "${DIRECTORIES[@]}" --max-depth=2 --type=d
+            fd . "${DIRECTORIES[@]}" --max-depth="$DEPTH" --type=d
         } |
             sed "s|^$HOME/||" |
             fzf --preview "lsd --tree --depth=1 --color=always --icon=always $HOME/{}"

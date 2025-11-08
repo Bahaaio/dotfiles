84b32569265e14cda34ef407b9733615748f65f6
Bahaa Mohamed <bahaa-dev@proton.me>
2025-11-09 01:33:23 +0200

    feat(tmux): update sessionizer and config
    - add home path to tmux-sessionizer
    - add sorting to tmux-sessionizer to remove duplicates (PATHS must end without /)
    - remove lazygit keybinding from tmux confing

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -2,19 +2,20 @@
 
 # Script to manage tmux sessions
 
-DIRECTORIES=(~/code ~/dotfiles ~/github ~/Documents) # to be searched for projects
-PATHS=(~/dotfiles ~/.config)                         # specific paths to include
-DEPTH=2                                              # depth for searching directories
+DIRECTORIES=(~/ ~/code ~/dotfiles ~/Documents) # to be searched for projects
+PATHS=(~/dotfiles ~/.config)                   # specific paths to include
+DEPTH=2                                        # depth for searching directories
 
 if [[ $# -eq 1 ]]; then
     selected="$1"
 else
     selected=$(
         {
-            printf '%s\n' "${PATHS[@]}"
+            printf '%s/\n' "${PATHS[@]}"
             fd . "${DIRECTORIES[@]}" --max-depth="$DEPTH" --type=d
         } |
             sed "s|^$HOME/||" |
+            sort -u |
             fzf --preview "lsd --tree --depth=1 --color=always --icon=always $HOME/{}"
     )
 

a00264aa019ab92cc7de528e72bcbd38301611b2
Bahaa Mohamed <bahaa-dev@proton.me>
2025-11-20 03:49:42 +0200

    feat(nvim): add golang testing

--- config/nvim/lua/plugins/golang.lua
+++ config/nvim/lua/plugins/golang.lua
@@ -15,4 +15,26 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-return {}
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "fredrikaverpil/neotest-golang",
+    },
+
+    config = function()
+      local neotest_golang_opts = {}
+
+      ---@diagnostic disable-next-line: missing-fields
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(neotest_golang_opts), -- Registration
+        },
+      })
+    end,
+  },
+}

cd2a683d7ed4eb567a374014604550dcf8030496
Bahaa Mohamed <bahaa-dev@proton.me>
2025-12-31 13:04:15 +0200

    fix: create target directories before symlinking

--- link.sh
+++ link.sh
@@ -5,6 +5,11 @@
 
 set -e
 
+# create required directories if they don't exist
+mkdir -p "$HOME/.local/bin"
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local/share/applications"
+
 stow zsh
 stow home
 stow bin --target="$HOME/.local/bin/"

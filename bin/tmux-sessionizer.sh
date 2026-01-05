55281badb772666a3279e7d716daa86bdde13735
Bahaa Mohamed <bahaa-dev@proton.me>
2026-01-05 14:32:00 +0200

    feat(tmux-sessionizer): remove trailing slash from session name
    - add comments

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -19,20 +19,27 @@ else
             fzf --preview "lsd --tree --depth=1 --color=always --icon=always $HOME/{}"
     )
 
-    [[ -n "$selected" ]] && selected="$HOME/$selected"
+    # prepend home directory and remove trailing slash
+    [[ -n "$selected" ]] && selected="$HOME/${selected%?}"
 fi
 
 [[ -z "$selected" ]] && exit 0
 
+# derive session name from selected path
+# replace dots with underscores
 session_name=$(basename "$selected" | tr . _)
 
+# if tmux is not running,
+# create or attach to tmux session
 if [[ -z "$TMUX" ]]; then
     exec tmux new -A -s "$session_name" -c "$selected"
     exit 0
 fi
 
+# create new tmux session if it doesn't exist
 if ! tmux has -t "$session_name" 2>/dev/null; then
     tmux new -ds "$session_name" -c "$selected"
 fi
 
+# switch to the session
 tmux switch-client -t "$session_name"

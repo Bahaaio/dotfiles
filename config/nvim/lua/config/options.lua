0c429a656c01863e870f8b2fd0733559f5765b8d
Bahaa Mohamed <bahaa-dev@proton.me>
2026-01-07 19:52:31 +0200

    refactor(nvim): remove redundant options
    - all removed options are set in lazyvim itself

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -9,17 +9,7 @@
 
 local opt = vim.o
 
-opt.cursorlineopt = "both"
-opt.relativenumber = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.wrap = false
-opt.spell = false
+opt.spell = true
 
-opt.expandtab = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.smartindent = true
-
--- overriding lazyvim's clipboard = "unnamedplus"
-opt.clipboard = ""
+-- don't use system clipboard
+vim.o.clipboard = ""

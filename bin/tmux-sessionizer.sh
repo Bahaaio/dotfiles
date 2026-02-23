9819f48f030dc2c59f73e0801f9b0ff9516c523e
Bahaa Mohamed <bahaa-dev@proton.me>
2026-02-23 20:53:19 +0200

    feat(sessionizer): add border and reverse flags

--- bin/tmux-sessionizer.sh
+++ bin/tmux-sessionizer.sh
@@ -7,20 +7,21 @@ PATHS=(~/dotfiles ~/.config)                   # specific paths to include
 DEPTH=2                                        # depth for searching directories
 
 if [[ $# -eq 1 ]]; then
-    selected="$1"
+  selected="$1"
 else
-    selected=$(
-        {
-            printf '%s/\n' "${PATHS[@]}"
-            fd . "${DIRECTORIES[@]}" --max-depth="$DEPTH" --type=d
-        } |
-            sed "s|^$HOME/||" |
-            sort -u |
-            fzf --preview "lsd --tree --depth=1 --color=always --icon=always $HOME/{}"
-    )
+  selected=$(
+    {
+      printf '%s/\n' "${PATHS[@]}"
+      fd . "${DIRECTORIES[@]}" --max-depth="$DEPTH" --type=d
+    } |
+      sed "s|^$HOME/||" |
+      sort -u |
+      fzf --reverse --border \
+        --preview "lsd --tree --depth=1 --color=always --icon=always $HOME/{}"
+  )
 
-    # prepend home directory and remove trailing slash
-    [[ -n "$selected" ]] && selected="$HOME/${selected%?}"
+  # prepend home directory and remove trailing slash
+  [[ -n "$selected" ]] && selected="$HOME/${selected%?}"
 fi
 
 [[ -z "$selected" ]] && exit 0
@@ -32,13 +33,13 @@ session_name=$(basename "$selected" | tr . _)
 # if tmux is not running,
 # create or attach to tmux session
 if [[ -z "$TMUX" ]]; then
-    exec tmux new -A -s "$session_name" -c "$selected"
-    exit 0
+  exec tmux new -A -s "$session_name" -c "$selected"
+  exit 0
 fi
 
 # create new tmux session if it doesn't exist
 if ! tmux has -t "$session_name" 2>/dev/null; then
-    tmux new -ds "$session_name" -c "$selected"
+  tmux new -ds "$session_name" -c "$selected"
 fi
 
 # switch to the session

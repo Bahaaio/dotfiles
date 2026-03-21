8e337d63a1ee864653da0707ff6811ab1d3e19fd
Bahaa Mohamed <bahaa-dev@proton.me>
2026-03-22 01:35:21 +0200

    feat(nvim): add nvim-java, tailwaind, typescript, and typst

--- config/nvim/lua/plugins/java.lua
+++ config/nvim/lua/plugins/java.lua
@@ -1,10 +1,27 @@
 return {
   {
-    "nvim-neotest/neotest",
-    dependencies = { "rcasia/neotest-java" },
+    "nvim-java/nvim-java",
+    ft = { "java", "yaml", "jproperties" },
+    config = function()
+      require("java").setup()
+      vim.lsp.enable("jdtls")
+    end,
+    keys = {
+      { "<leader>j", group = "Java" },
+      { "<leader>jp", "<cmd>JavaProfile<Cr>", desc = "profiles UI" },
+      { "<leader>jr", "<cmd>JavaRunnerRunMain<Cr>", desc = "run main" },
+      { "<leader>js", "<cmd>JavaRunnerStopMain<Cr>", desc = "stop main" },
+      { "<leader>jl", "<cmd>JavaRunnerToggleLogs<Cr>", desc = "toggle logs" },
+      { "<leader>jc", "<cmd>JavaBuildCleanWorkspace<Cr>", desc = "clean build" },
+      { "<leader>jb", "<cmd>JavaBuildBuildWorkspace<Cr>", desc = "build workspace" },
+    },
+  },
+
+  {
+    "folke/which-key.nvim",
     opts = {
-      adapters = {
-        ["neotest-java"] = {},
+      spec = {
+        { "<leader>j", group = "Java" },
       },
     },
   },

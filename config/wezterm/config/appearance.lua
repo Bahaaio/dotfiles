8bf97856c234bcde214d22fbcb3c720b4c66db90
Bahaa Mohamed <bahaa-dev@proton.me>
2026-03-22 21:49:58 +0200

    refactor(wez): remove bottom padding

--- config/wezterm/config/appearance.lua
+++ config/wezterm/config/appearance.lua
@@ -24,7 +24,7 @@ return {
 	-- window_background_opacity = 0.85,
 	window_close_confirmation = "NeverPrompt",
 	adjust_window_size_when_changing_font_size = false,
-	-- window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+	window_padding = { top = 10, bottom = 0 },
 
 	-- cursor
 	animation_fps = 1,

c1ee81cc241b886e7e6dec472b6ceebe5ada7df5
Bahaa Mohamed <bahaa-dev@proton.me>
2026-04-25 02:02:08 +0300

    chore: remove apps

--- link.sh
+++ link.sh
@@ -8,10 +8,8 @@ set -e
 # create required directories if they don't exist
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.config"
-mkdir -p "$HOME/.local/share/applications"
 
 stow zsh
 stow home
 stow bin --target="$HOME/.local/bin/"
 stow config --target="$HOME/.config/"
-stow apps --target="$HOME/.local/share/applications/"

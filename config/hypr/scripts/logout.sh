70274847d934c648b40ac3024fbd514fda8f73f4
Bahaa Mohamed <bahaa-dev@proton.me>
2026-05-21 20:19:49 +0300

    refactor(hyprland): misc
    - change touchpad scroll factor to 0.4
    - change touchpad scroll factor in wezterm to 0.025
    - use preferred resolution for monitor
    - don't screen share zapzap
    - don't focus ticktick on startup

--- config/hypr/scripts/logout.sh
+++ config/hypr/scripts/logout.sh
@@ -4,8 +4,4 @@ nohup bash -c "sleep 2 && uwsm stop" >/dev/null 2>&1 &
 
 # close all windows
 hyprctl clients -j | jq -r ".[].address" | xargs -I{} hyprctl dispatch closewindow address:{}
-
-# Move to first workspace
-hyprctl dispatch workspace 1
-
-sleep 1 # Allow apps like Chrome to shutdown correctly
+sleep 1

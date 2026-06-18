f9128f54b24623e26c938e2893e9bb740f8ee104
Bahaa Mohamed <bahaa-dev@proton.me>
2026-06-19 01:01:18 +0300

    refactor(bin): pipe output of open-remote.sh to /dev/null

--- bin/open-remote.sh
+++ bin/open-remote.sh
@@ -3,4 +3,4 @@
 # opens the remote URL of the git repository in the current tmux pane
 
 cd "$(tmux run "echo #{pane_start_path}")" || exit 1
-xdg-open "$(git remote get-url origin)" || echo "No remote found"
+xdg-open "$(git remote get-url origin)" >/dev/null || echo "Failed to open remote URL"

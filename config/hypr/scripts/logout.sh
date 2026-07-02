373ee7ee9308766dea8106aac87da01dcf125adb
Bahaa Mohamed <bahaa-dev@proton.me>
2026-07-02 06:02:38 +0300

    refactor: use uwsm stop instead of logout.sh
    - sddm crashes when using loginctl terminate-user/session

--- config/hypr/scripts/logout.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nohup bash -c "sleep 2 && uwsm stop" >/dev/null 2>&1 &
-
-# close all windows
-hyprctl clients -j | jq -r ".[].address" | xargs -I{} hyprctl dispatch 'hl.dsp.window.close("address:{}")'
-sleep 1

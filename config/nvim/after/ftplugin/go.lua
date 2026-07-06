49be0afff179e96d4a22206f4215bb2ac0995eed
Bahaa Mohamed <bahaa-dev@proton.me>
2026-07-06 18:03:16 +0300

    refactor(nvim): move golang config to ftplugin

---
+++ config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,10 @@
+local go_term = require("toggleterm.terminal").Terminal:new({
+  cmd = "go run .",
+  close_on_exit = false,
+  direction = "float",
+})
+
+vim.keymap.set("n", "<leader>R", function()
+  vim.cmd("write")
+  go_term:toggle()
+end, { noremap = true, silent = true, desc = "Run golang project" })

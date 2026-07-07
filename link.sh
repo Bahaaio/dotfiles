bb2d0af0f79052a7e19c44fe7b59af9488bc72e6
Bahaa Mohamed <bahaa-dev@proton.me>
2026-07-07 15:02:46 +0300

    chore(link.sh): don't stow home/

--- link.sh
+++ link.sh
@@ -6,10 +6,9 @@
 set -e
 
 # create required directories if they don't exist
-mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.local/bin"
 
 stow zsh
-stow home
 stow bin --target="$HOME/.local/bin/"
 stow config --target="$HOME/.config/"

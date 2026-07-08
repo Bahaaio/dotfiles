bffd35157332320021226231373e2a97ad42de06
Bahaa Mohamed <bahaa-dev@proton.me>
2026-07-08 04:38:33 +0300

    refactor(nvim): simplify lualine

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -38,9 +38,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 1 } },
-        },
+        lualine_y = { "progress" },
       },
     },
   },

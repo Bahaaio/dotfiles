9ffc3fbd832912e076d9b9dd5804e5d82609883f
Bahaa Mohamed <bahaa-dev@proton.me>
2026-07-25 18:56:57 +0300

    feat(nvim): lazy.nvim ui border

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -31,6 +31,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight" } },
+  ui = {
+    border = "rounded",
+    backdrop = 100,
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
